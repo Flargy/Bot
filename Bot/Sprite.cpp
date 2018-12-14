b1106af0156f31804572c3f88a0b7025a08252da
Flargy <marlun1996@hotmail.com>
2018-12-14 11:16:25 +0100

    sdl2.dll

--- Bot/Sprite.cpp
+++ Bot/Sprite.cpp
@@ -1,12 +1,15 @@
 #include "Sprite.h"
 #include "Window.h"
+#include <SDL_image.h>
 
 
 namespace Bot {
+	Window winclass;
 
 	Sprite::Sprite(const char* path, int x, int y)
 	{
-	//	SDL_Surface surf = SDL_CreateRGBSurface()
+		SDL_Surface* surf = IMG_Load(path);
+		SDL_Texture* tx = SDL_CreateTextureFromSurface(winclass.getRen(), surf);
 	}
 
 	/* Saker som classen kommer behöva
